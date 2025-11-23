ed1467ef6292b600ace73e343cc0ae55c5aaa439
mtm_007 <45622655+mtm-007@users.noreply.github.com>
2025-11-23 09:59:48 +0000

    make todo list interactive toggle done and delete options

--- fasthtml/main.py
+++ fasthtml/main.py
@@ -1,18 +1,33 @@
 from fasthtml.common import *
 #from fasthtml import common as fh
 
+def render(todo):
+    tid = f'todo-{todo.id}'
+    toggle = A('Toggle', hx_get=f'/toggle/{todo.id}', target_id=tid)
+    delete = A('Delete', hx_delete=f'/{todo.id}',
+               hx_swap='outerHTML', target_id=tid)
+    return Li(toggle, delete, todo.title + (' ✅' if todo.done else ''),
+              id=tid)
 
 app, rt, todos, Todo = fast_app(
-        'todos.db',live=True, 
-        tbls={"todos":dict(id=int, title=str, done=bool, pk='id')})
+        'todos.db', live=True, 
+        tbls={"todos":dict(id=int, title=str, render=render, done=bool, pk='id')})
 
 @rt('/')
 def get():
-    todos.insert(Todo(title="second todo", done=False))
-    #items = todos()
-    items = [Li(o) for o in todos()]
+    #todos.insert(Todo(title="second todo", done=False))
+    #items = [Li(o) for o in todos()]
     return Titled('Todos List',
-                  Ul(*items),
+                  Ul(*todos()),
                   )
+
+@rt('/{tid}')
+def delete(tid:int):todos.delete(tid)
+
+@rt('/toggle/{tid}')
+def get(tid:int):
+    todo = todos[tid]
+    todo.done = not todo.done
+    return todos.update(todo)
 
 serve()
