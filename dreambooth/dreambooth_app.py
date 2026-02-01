bb147d36732972df3caacb769ac47b39282d3c7c
mtm_007 <45622655+mtm-007@users.noreply.github.com>
2026-02-01 13:12:25 +0000

    full script for dreambooth app

--- dreambooth/dreambooth_app.py
+++ dreambooth/dreambooth_app.py
@@ -9,6 +9,10 @@ from smart_open import open
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+import subprocess
+from accelerate.utils import write_basic_config
+import gradio as gr
+from gradio.routes import mount_gradio_app
 
 from modal import ( App, Image, Mount, Secret, Volume, asgi_app, enter, method)
 
@@ -21,10 +25,10 @@ GIT_SHA = ( "abd922bd0c43a504e47eca2ed354c3634bd00834")  # specify the commit to
 
 image = (image.apt_install("git")
          .run_commands(
-             "cd /root && git init .",
-             "cd /root && git remote add origin https://github.com/huggingface/diffusers",
-        f"cd /root && git fetch --depth=1 origin {GIT_SHA} && git checkout {GIT_SHA}",
-        "cd /root && pip install -e .",
+            "cd /root && git init .",
+            "cd /root && git remote add origin https://github.com/huggingface/diffusers",
+            f"cd /root && git fetch --depth=1 origin {GIT_SHA} && git checkout {GIT_SHA}",
+            "cd /root && pip install -e .",
          ))
 
 @dataclass
@@ -47,11 +51,10 @@ def download_models():
 
 image = image.run_function(download_models)
 
-Volume = Volume.from_name( "dreambooth-finetunning-volume", create_if_missing=True)
+volume = Volume.from_name( "dreambooth-finetunning-volume", create_if_missing=True)
 MODEL_DIR = "/model"
 
 #load dataset
-
 def load_images(image_urls: list[str]) -> Path:
     img_path = Path("/img")
 
@@ -66,10 +69,156 @@ def load_images(image_urls: list[str]) -> Path:
 
 USE_WANDB = False
 
-
 @dataclass
 class TrainConfig(SharedConfig):
     """ Configuration for finetunining steps."""
+    # training prompt looks like `{PREFIX} {INSTANCE_NAME} the {CLASS_NAME} {POSTFIX}`
     prefix: str = "a photo of"
     postfix: str = ""
+
+    # locator for plaintext file with urls for images of target instance
+    instance_example_urls_file: str = ( Path(__file__).parent / "instance_example_urls.txt")
+
+    #hyperparameters
+    resolution: int = 1024
+    train_batch_size: int = 4
+    gradient_accumulation_steps: int = 1
+    learning_rate: float = 1e-4
+    lr_scheduler:str = "constant"
+    lr_warmup_steps: int = 0
+    max_train_steps: int = 80
+    checkpointing_steps: int = 1000
+    seed: int = 117
+
+@app.function(image=image, gpu="A100", volumes={MODEL_DIR:volume}, timeout=1800, secrets=[Secret.from_name("my-wandb-secret")] if USE_WANDB else [],)
+
+def train(instance_example_urls):
+    config = TrainConfig()
+    #load data locally
+    img_path = load_images(instance_example_urls)
+
+    write_basic_config(mixed_precision="fp16")
+
+    #define training prompt
+    instance_phrase = f"{config.instance_name} the {config.class_name}"
+    prompt = f"{config.prefix} {instance_phrase} { config.postfix}".strip()
+
+    # the model training is packaged as a script, so we have to execute it as a subprocess, which adds some boilerplate
+    def _exec_subprocess(cmd: list[str]):
+        """Executes subprocess and prints log to terminal while subprocess is running."""
+        process = subprocess.Popen( cmd, stdout= subprocess.PIPE, stderr=subprocess.STDOUT,)
+        with process.stdout as pipe:
+            for line in iter(pipe.readline, b""):
+                line_str = line.decode()
+                print(f"{line_str}", end="")
+        
+        if exitcode := process.wait() != 0:
+            raise subprocess.CalledProcessError(exitcode, "\n".join(cmd))
     
+    print("launching dreambooth training script")
+    _exec_subprocess( [ "accelerate", "launch", "examples/dreambooth/train_dreambooth_lora_sdxl.py",
+                        "--mixed_precision=fp16",
+                        f"--pretrained_model_name_or_path={config.model_name}",
+                        f"--pretrained_vae_model_name_or_path={config.vae_name}",
+                        f"--instance_data_dir={img_path}",
+                        f"--output_dir={MODEL_DIR}",
+                        f"--instance_prompt={prompt}",
+                        f"--resolution={config.resolution}",
+                        f"--train_batch_size={config.train_batch_size}",
+                        f"--gradient_accumulation_steps={config.gradient_accumulation_steps}",
+                        f"--learning_rate={config.learning_rate}",
+                        f"--lr_scheduler={config.lr_scheduler}",
+                        f"--lr_warmup_steps={config.lr_warmup_steps}",
+                        f"--max_train_steps={config.max_train_steps}",
+                        f"--checkpointing_steps={config.checkpointing_steps}",
+                        f"--seed={config.seed}",]
+                    + (
+                        ["--report_to=wandb", f"--validation_prompt={prompt} in space",
+                        f"--validation_epochs={config.max_train_steps // 5}",] if USE_WANDB else []
+                      ),
+                    )
+    volume.commit()
+
+@app.cls(image=image, gpu="A10G", volumes={MODEL_DIR: volume})
+class Model:
+    #enter()
+    def load_model(self):
+        
+        config = TrainConfig()
+        volume.reload()
+
+        pipe = DiffusionPipeline.from_pretrained(
+            config.model_name,  vae=AutoencoderKL.from_pretrained(config.vae_name, torch_dtype=torch.float16),
+                                torch_dtype=torch.float16,).to("cuda")
+        pipe.load_lora_weights(MODEL_DIR)
+        self.pipe = pipe
+    
+    @method()
+    def inference(self, text, config):
+        return self.pipe(  text, num_inference_steps=config.num_inference_steps, guidance_scale=config.guidance_scale,).images[0]
+        
+web_app = FastAPI()
+assets_path = Path(__file__).parent / "assets" 
+
+@dataclass
+class AppConfig(SharedConfig):
+    """ Configuration information for inference."""
+    num_inference_steps: int = 25
+    guidance_scale: float = 7.5
+
+@app.function(  image=image, concurrency_limit=3, mounts =[Mount.from_local_dir(assets_path, remote_path="/assets")],)
+
+@asgi_app()
+def fastapi_app():
+    # Call out to the inference in a separate Modal environment with a GPU
+    def go(text=""):
+        if not text:
+            text = example_prompts[0]
+        return Model().inference.remote(text, config)
+
+        #set up AppConfig
+    config = AppConfig()
+
+    instance_phrase = f"{config.instance_name} the {config.class_name}"
+    example_prompts = [ f"{instance_phrase}",
+                        f"a painting of {instance_phrase.title()} With A Pearl Earring, by Vermeer",
+                        f"oil painting of {instance_phrase} flying through space as an astronaut",
+                        f"a painting of {instance_phrase} in cyberpunk city. character desing by cory loftis.volumetric light, detailed, rendered in octance",
+                        f"drawing of {instance_phrase} high quality, cartoon, path traced, by studio ghibli and don bluth",]
+    modal_docs_url = "https://modal.com/docs/guide"
+    modal_example_url = f"{modal_docs_url}/examples/dreambooth_app"
+    description = f"""Describe what they are doing or how a particular artist or style would depict them. Be fantastical! Try the examples below for inspiration.
+
+    ### Learn how to make a "Dreambooth" for your own pet [here]({modal_example_url}).
+    """
+    #custom styles: an icon, a background, and theme
+    @web_app.get("/favicon.ico", include_in_schema=False)
+    async def favicon(): 
+        return FileResponse("/assets/favicon.svg")
+
+    with open("/assets/index.css") as f: css = f.read()
+
+    theme = gr.themes.Default(  primary_huw="green", secondary_hue="emerald", neutral_hue="neutral" )
+
+    #add gradio UI around inference
+    with gr.Blocks( theme = theme, css=css, title ="Pet Dreambooth on Modal") as interface:
+        gr.Markdown( f"# Dream up images of {instance_phrase}.\n\n{description}",)
+        with gr.Row():
+            inp = gr.Textbox ( label="", placeholder=f"Describe the version of {instance_phrase} you'd like to see", lines=10,)
+            out = gr.Image( height=512, width=512, label="", min_width=512, elem_id="output")
+        with gr.Row():
+            btn = gr.Button("Dream", variant="primary", scale=2)
+            btn.click(  fn=go, inputs=inp, outputs=out) # connect inputs and outputs with inference function
+            gr.Button(  "⚡️ Powered by Modal", variant ="secondary", link="https://modal.com",)
+        with gr.Column(variant="compact"):
+            for ii, prompt in enumerate(example_prompts):
+                btn = gr.Button(prompt, variant="secondary")
+                btn.click(fn=lambda idx=ii: example_prompts[idx], outputs=inp)
+    return mount_gradio_app(app=web_app, blocks=interface, path="/")
+
+
+@app.local_entrypoint()
+def run():
+    with open(TrainConfig().instance_example_urls_file) as f:
+        instance_example_urls = [line.strip() for line in f.readlines()]
+    train.remote(instance_example_urls)
