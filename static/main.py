fee296c26d1791588acc4c7e23a319ce6e996f8c
mtm_007 <45622655+mtm-007@users.noreply.github.com>
2026-02-27 07:27:16 +0000

    adding blog card and blog posts list cards with images

--- static/main.py
+++ static/main.py
@@ -1,11 +1,25 @@
 import fasthtml.common as fh
 import monsterui.all as mui
-import os 
+import os,yaml
 
 app = fh.FastHTML(hdrs=mui.Theme.blue.headers(), live=True)
 
+def BlogCard(fname):
+    with open(f"posts/{fname}", "r") as f:content = f.read()
+    meta = content.split("---")[1]
+    meta = yaml.safe_load(meta)
+        
+    return mui.Card(mui.DivHStacked(
+                    fh.Img(src=meta["image"]),
+                    fh.Div(
+                        fh.H3(meta["title"]), 
+                        fh.P(meta["description"]),
+                        fh.P(meta["author"]),
+                        fh.P(meta["date"]),
+                        fh.P(meta["categories"]))
+                    ))
 @app.route("/")
 def index():
-    return map(mui.Card, os.listdir("posts"))
+    return map(BlogCard, os.listdir("posts"))
 
 fh.serve()
---
+++ static/posts/why.md
@@ -0,0 +1,10 @@
+---
+title: "Getting Started with My Blog"
+description: "Welcome to my first blog post! Lets check it out"
+author: "Merhawi"
+date: "2026-02-26"
+categories: ["Blog", "Getting Started", "Introduction"]
+image: "https://picsum.photos/400/200?random=12"
+---
+
+## Welcome to the page
