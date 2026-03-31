6257afc0439d863464dd43ae5f679ab22f6be02a
mtm_007 <45622655+mtm-007@users.noreply.github.com>
2026-03-31 21:42:47 +0000

    add navigation nav bar

--- checkboxes_v0/main_page/main.py
+++ checkboxes_v0/main_page/main.py
@@ -3,8 +3,14 @@ import monsterui.all as mui
 
 app, rt = fh.fast_app(hdrs=mui.Theme.blue.headers(), live=True)
 
+def NavBar():
+    return mui.NavBar(fh.A("Blog", href="/blog"),
+                      fh.A("Team", href="/team"),
+                      fh.A("Services", href="/services"),
+                      brand=fh.H3("Agent Unlock"))
+
 @rt
 def index():
-    return "Hello World!"
+    return NavBar()
 
 fh.serve()
