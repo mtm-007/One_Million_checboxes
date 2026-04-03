e23c70701dcd1eb4a9778fe88eef2cfe02d01a48
mtm_007 <45622655+mtm-007@users.noreply.github.com>
2026-04-03 23:50:06 +0000

    add Team Card, dev mode live code, having issues due to conflict between fasthtml and startlette

--- checkboxes_v0/main_page/main.py
+++ checkboxes_v0/main_page/main.py
@@ -1,7 +1,7 @@
 import fasthtml.common as fh
 import monsterui.all as mui 
 
-app, rt = fh.fast_app(hdrs=mui.Theme.blue.headers(), live=True)
+app, rt = fh.fast_app(hdrs=mui.Theme.blue.headers())#, live=True)
 
 def NavBar():
     return mui.NavBar(fh.A("Blog", href="/blog"),
@@ -10,9 +10,26 @@ def NavBar():
                       fh.A("Theme", href=theme),
                       brand=fh.H3("Agent Unlock Labs"))
 
-@rt
+def TeamCard(name, role):#, location="Remote"):
+        return fh.Card(
+            mui.DivLAligned(
+                mui.DiceBearAvatar(name, h=24, w=24),
+                fh.Div(fh.H3(name), fh.P(role))),
+            footer=mui.DivFullySpaced(
+                #mui.DivHStacked(mui.UkIcon("map-pin", height=16), fh.P(location)),
+                mui.DivRAligned(*(mui.UkIconLink(icon, height=16) for icon in ("mail", "linkedin", "github", "rss", "globe")))) )
+ 
+
+#     return fh.Grid(*team, cols_sm=1, cols_md=1, cols_lg=2, cols_xl=3)
+
+
+@rt("/")
 def team():
-    return  NavBar(), fh.Titled("Team", fh.P("Agent Unlock Team"))
+    return  NavBar(), fh.Titled("Team", fh.P("Agent Unlock Team"), 
+            fh.Grid(TeamCard("Agent Name", "Founder"),#, "Remote, US"),
+                    TeamCard("Agent Name", "Founder"),#, "Remote, US"),
+                    TeamCard("Agent Name", "Founder")#, "Remote, US"),
+                             ))
 
 @rt("/theme")
 def theme():
